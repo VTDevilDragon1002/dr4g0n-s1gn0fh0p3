16374a5d82847cb5eb5a963e877fb99db6db89d3
Vishal T <vishaldcp614@gmail.com>
2026-06-05 21:15:59 +0530

    Delete s1gn0fh0p3 directory

--- s1gn0fh0p3/start-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Sign of Hope Local Server
-cd /d "%~dp0"
-echo Starting Sign of Hope on http://localhost:5500
-echo Keep this window open while using camera/microphone features.
-python -m http.server 5500
-pause
--- s1gn0fh0p3/start-server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-echo "Starting Sign of Hope on http://localhost:5500"
-python3 -m http.server 5500
